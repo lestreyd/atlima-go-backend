f8465f57aa3b3e921e8baefb93c629a6487e90a1
lestreyd <flestreyd@gmail.com>
2022-08-07 21:10:41 +0300

    new PromoCode builder

--- slot.go
+++ slot.go
@@ -21,8 +21,83 @@ type PromoCodeInterface interface {
 	Title(title string)
 	Discount(discount float32)
 	Limit(limit int)
-	Finished(finishDate time.Time)
+	Finish(finishDate time.Time)
 	WithMeta(meta Meta)
+}
+
+type PromoCodeBuilder struct {
+	promoCode *PromoCode
+}
+
+func (b *PromoCodeBuilder) AvailableBy(id int) *PromoCodeIdBuilder {
+	// add id for PromoCode
+	b.promoCode.id = id
+	return &PromoCodeIdBuilder{*b}
+}
+
+type PromoCodeIdBuilder struct {
+	// builder for id
+	PromoCodeBuilder
+}
+
+func (b *PromoCodeBuilder) Title(title string) *PromoCodeTitleBuilder {
+	// add title
+	b.promoCode.title = title
+	return &PromoCodeTitleBuilder{*b}
+}
+
+type PromoCodeTitleBuilder struct {
+	// builder for title
+	PromoCodeBuilder
+}
+
+func (b *PromoCodeBuilder) Discount(discount float32) *PromoCodeDiscountBuilder {
+	// add discount to PromoCode
+	b.promoCode.discount = discount
+	return &PromoCodeDiscountBuilder{*b}
+}
+
+type PromoCodeDiscountBuilder struct {
+	// builder for discount field
+	PromoCodeBuilder
+}
+
+func (b *PromoCodeBuilder) Limit(limit int) *PromoCodeLimitBuilder {
+	// add limit
+	b.promoCode.limit = limit
+	return &PromoCodeLimitBuilder{*b}
+}
+
+type PromoCodeLimitBuilder struct {
+	// builder for limit
+	PromoCodeBuilder
+}
+
+func (b *PromoCodeBuilder) Finish(finishDate time.Time) *PromoCodeFinishBuilder {
+	// add finishDate. If nil - not installed
+	b.promoCode.finishDate = finishDate
+	return &PromoCodeFinishBuilder{*b}
+}
+
+type PromoCodeFinishBuilder struct {
+	// builder for promoCode
+	PromoCodeBuilder
+}
+
+func (b *PromoCodeBuilder) WithMeta(meta Meta) *PromoCodeMetaBuilder {
+	// add meta to PromoCode
+	b.promoCode.meta = meta
+	return &PromoCodeMetaBuilder{*b}
+}
+
+type PromoCodeMetaBuilder struct {
+	// builder for PromoCode Meta
+	PromoCodeBuilder
+}
+
+func (b *PromoCodeBuilder) Build() *PromoCode {
+	// Build PromoCode object
+	return b.promoCode
 }
 
 type Squad struct {
@@ -44,6 +119,10 @@ type SquadInterface interface {
 	Blocked(blocked bool)
 	WithPassword(password string)
 	WithMeta(meta Meta)
+}
+
+type SquadBuilder struct {
+	squad *Squad
 }
 
 type Team struct {
