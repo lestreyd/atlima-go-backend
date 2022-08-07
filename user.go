095adfc12a991c21634858e0428096d376972700
lestreyd <flestreyd@gmail.com>
2022-08-07 17:10:32 +0300

    add User and Weapon builders

--- user.go
+++ user.go
@@ -65,3 +65,251 @@ type UserInterface interface {
 	InCity(city City)
 	WithMeta(meta Meta)
 }
+
+type UserBuilder struct {
+	user *User
+}
+
+func (b *UserBuilder) AvailableBy() *UserAvailabilityBuilder {
+	// set all availability parameters
+	return &UserAvailabilityBuilder{*b}
+}
+
+type UserAvailabilityBuilder struct {
+	UserBuilder
+}
+
+func (b *UserBuilder) Id(id int) *UserIdBuilder {
+	// set id for user
+	b.user.id = id
+	return &UserIdBuilder{*b}
+}
+
+type UserIdBuilder struct {
+	UserBuilder
+}
+
+func (b *UserBuilder) Slug(slug string) *UserSlugBuilder {
+	// set slug for user
+	b.user.slug = slug
+	return &UserSlugBuilder{*b}
+}
+
+type UserSlugBuilder struct {
+	UserBuilder
+}
+
+func (b *UserBuilder) WithContacts(contacts Contacts) *UserContactsBuilder {
+	// set contact for User
+	b.user.contacts = contacts
+	return &UserContactsBuilder{*b}
+}
+
+type UserContactsBuilder struct {
+	UserBuilder
+}
+
+func (b *UserBuilder) Set() *UserPropertiesBuilder {
+	// set user properties
+	return &UserPropertiesBuilder{*b}
+}
+
+type UserPropertiesBuilder struct {
+	UserBuilder
+}
+
+func (b *UserBuilder) Sex(sex int) *UserSexBuilder {
+	// set user sex
+	b.user.sex = sex
+	return &UserSexBuilder{*b}
+}
+
+type UserSexBuilder struct {
+	UserBuilder
+}
+
+func (b *UserBuilder) Privacy(privacy PrivacyConfiguration) *UserPrivacyBuilder {
+	// set privacy configuration
+	b.user.privacy = privacy
+	return &UserPrivacyBuilder{*b}
+}
+
+type UserPrivacyBuilder struct {
+	UserBuilder
+}
+
+func (b *UserBuilder) Qualifications(qualifications []Qualification) *UserQualificationBuilder {
+	// set qualifications for user
+	for i := range qualifications {
+		b.user.qualifications = append(b.user.qualifications, qualifications[i])
+	}
+	return &UserQualificationBuilder{*b}
+}
+
+type UserQualificationBuilder struct {
+	UserBuilder
+}
+
+func (b *UserBuilder) StrongHand(strongHand int) *UserStrongHandBuilder {
+	// set strong hand
+	b.user.strongHand = strongHand
+	return &UserStrongHandBuilder{*b}
+}
+
+type UserStrongHandBuilder struct {
+	UserBuilder
+}
+
+func (b *UserBuilder) BirthDate(birthDate time.Time) *UserBirthDateBuilder {
+	// set birthDate for user
+	b.user.birthDate = birthDate
+	return &UserBirthDateBuilder{*b}
+}
+
+type UserBirthDateBuilder struct {
+	UserBuilder
+}
+
+func (b *UserBuilder) LastLogin(lastLogin time.Time) *UserLastLoginBuilder {
+	// set last login datetime for user
+	b.user.lastLogin = lastLogin
+	return &UserLastLoginBuilder{*b}
+}
+
+type UserLastLoginBuilder struct {
+	UserBuilder
+}
+
+func (b *UserBuilder) Image(image []byte) *UserImageBuilder {
+	// set image for user
+	b.user.image = image
+	return &UserImageBuilder{*b}
+}
+
+type UserImageBuilder struct {
+	UserBuilder
+}
+
+func (b *UserBuilder) Role(image []byte) *UserRoleBuilder {
+	// set role for user
+	b.user.image = image
+	return &UserRoleBuilder{*b}
+}
+
+type UserRoleBuilder struct {
+	UserBuilder
+}
+
+func (b *UserBuilder) Active(active bool) *UserActivityBuilder {
+	// set user active/non-active status
+	b.user.active = active
+	return &UserActivityBuilder{*b}
+}
+
+type UserActivityBuilder struct {
+	UserBuilder
+}
+
+func (b *UserBuilder) Password(password string) *UserPasswordBuilder {
+	// set password for user (salted)
+	b.user.password = password
+	return &UserPasswordBuilder{*b}
+}
+
+type UserPasswordBuilder struct {
+	UserBuilder
+}
+
+func (b *UserBuilder) AuthToken(authToken string) *UserAuthTokenBuilder {
+	// set auth token for user
+	b.user.authToken = authToken
+	return &UserAuthTokenBuilder{*b}
+}
+
+type UserAuthTokenBuilder struct {
+	UserBuilder
+}
+
+func (b *UserBuilder) RefreshToken(refreshToken string) *UserRefreshTokenBuilder {
+	// set refresh token for oauth2
+	b.user.refreshToken = refreshToken
+	return &UserRefreshTokenBuilder{*b}
+}
+
+type UserRefreshTokenBuilder struct {
+	UserBuilder
+}
+
+func (b *UserBuilder) DeviceId(deviceId string) *UserDeviceIdBuilder {
+	// set user device id
+	b.user.deviceId = deviceId
+	return &UserDeviceIdBuilder{*b}
+}
+
+type UserDeviceIdBuilder struct {
+	UserBuilder
+}
+
+func (b *UserBuilder) IpAddress(ipAddress string) *UserIpAddressBuilder {
+	// set user ip address
+	b.user.ipAddress = ipAddress
+	return &UserIpAddressBuilder{*b}
+}
+
+type UserIpAddressBuilder struct {
+	UserBuilder
+}
+
+func (b *UserBuilder) Location() *UserLocationBuilder {
+	// set user Location
+	return &UserLocationBuilder{*b}
+}
+
+type UserLocationBuilder struct {
+	UserBuilder
+}
+
+func (b *UserBuilder) InCountry(country Country) *UserCountryBuilder {
+	// set user Country
+	b.user.country = country
+	return &UserCountryBuilder{*b}
+}
+
+type UserCountryBuilder struct {
+	UserBuilder
+}
+
+func (b *UserBuilder) InRegion(region Region) *UserRegionBuilder {
+	// set user Region
+	b.user.region = region
+	return &UserRegionBuilder{*b}
+}
+
+type UserRegionBuilder struct {
+	UserBuilder
+}
+
+func (b *UserBuilder) InCity(city City) *UserCityBuilder {
+	// set user City
+	b.user.city = city
+	return &UserCityBuilder{*b}
+}
+
+type UserCityBuilder struct {
+	UserBuilder
+}
+
+func (b *UserBuilder) WithMeta(meta Meta) *UserMetaBuilder {
+	// build meta for user
+	b.user.meta = meta
+	return &UserMetaBuilder{*b}
+}
+
+type UserMetaBuilder struct {
+	UserBuilder
+}
+
+func (b *UserBuilder) Build() *User {
+	// UserBuilder object
+	return b.user
+}
--- weapon.go
+++ weapon.go
@@ -5,13 +5,64 @@ type Weapon struct {
 	// in IPSC
 	id            int
 	image         []byte
-	abbreviations []Content
+	abbreviations []string
 	meta          Meta
 }
 
 type WeaponInterface interface {
 	AvailableBy(id int)
 	Image(image []byte)
-	Abbreviations(abbreviations []Content)
+	Abbreviations(abbreviations []string)
 	WithMeta(meta Meta)
+}
+
+type WeaponBuilder struct {
+	weapon *Weapon
+}
+
+func (b *WeaponBuilder) AvailableBy(id int) *WeaponIdBuilder {
+	// build availability by id
+	b.weapon.id = id
+	return &WeaponIdBuilder{*b}
+}
+
+type WeaponIdBuilder struct {
+	WeaponBuilder
+}
+
+func (b *WeaponBuilder) Image(image []byte) *WeaponImageBuilder {
+	// build meta for user
+	b.weapon.image = image
+	return &WeaponImageBuilder{*b}
+}
+
+type WeaponImageBuilder struct {
+	WeaponBuilder
+}
+
+func (b *WeaponBuilder) Abbreviations(abbreviations []string) *WeaponAbbreviationsBuilder {
+	// build abbreviations list
+	for i := range abbreviations {
+		b.weapon.abbreviations = append(b.weapon.abbreviations, abbreviations[i])
+	}
+	return &WeaponAbbreviationsBuilder{*b}
+}
+
+type WeaponAbbreviationsBuilder struct {
+	WeaponBuilder
+}
+
+func (b *WeaponBuilder) Meta(meta Meta) *WeaponMetaBuilder {
+	// build meta for user
+	b.weapon.meta = meta
+	return &WeaponMetaBuilder{*b}
+}
+
+type WeaponMetaBuilder struct {
+	WeaponBuilder
+}
+
+func (b *WeaponBuilder) Build() *Weapon {
+	// WeaponBuilder object
+	return b.weapon
 }
